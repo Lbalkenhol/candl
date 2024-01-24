785c2acbe5c5aa5a74740f0894c763dfa32946e9
Lennart Balkenhol <lennart@MacBook-Pro-90.local>
2024-01-24 15:15:54 +0100

    the struggle continues

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,7 @@ sys.path.insert(0, os.path.abspath('./../..'))
 sys.path.insert(0, os.path.abspath('./..'))
 sys.path.insert(0, os.path.abspath('./../candl'))
 sys.path.insert(0, os.path.abspath('./candl'))
+sys.path.insert(0, os.path.abspath("."))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
