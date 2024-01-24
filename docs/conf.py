2d66075fa4e56ac0c2f4a331cac924b2f186d43a
Lennart Balkenhol <lennart@MacBook-Pro-90.local>
2024-01-24 15:12:27 +0100

    struggling with autodocs

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('./../..'))
+sys.path.insert(0, os.path.abspath('./..'))
+sys.path.insert(0, os.path.abspath('./../candl'))
+sys.path.insert(0, os.path.abspath('./candl'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
