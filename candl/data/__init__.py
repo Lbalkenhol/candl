481d20b8e119a8e41c94fad5111b9d2dd42942a2
Lennart Balkenhol <lennart@MacBook-Pro-90.local>
2024-02-22 15:36:32 +0100

    Added more info about data sets

--- candl/data/__init__.py
+++ candl/data/__init__.py
@@ -24,3 +24,22 @@ SPT3G_2018_Lens_and_CMB = f"{data_path}/SPT3G_2018_Lens/SPT3G_2018_PP_lens_and_C
 ACT_DR4_TTTEEE = f"{data_path}/ACT_DR4_CMB_only/ACT_DR4_CMB_only.yaml"
 ACT_DR6_Lens = f"{data_path}/ACT_DR6_Lens/ACT_DR6_KK_lens_only.yaml"
 ACT_DR6_Lens_and_CMB = f"{data_path}/ACT_DR6_Lens/ACT_DR6_KK_lens_and_CMB.yaml"
+
+shortcuts = {
+    "SPT-3G 2018 TT/TE/EE": "SPT3G_2018_TTTEEE",
+    "SPT-3G 2018 Lensing": "SPT3G_2018_Lens",
+    "SPT-3G 2018 Lensing (for combining with primary CMB)": "SPT3G_2018_Lens_and_CMB",
+    "ACT DR4 TT/TE/EE": "ACT_DR4_TTTEEE",
+    "ACT DR6 Lensing": "ACT_DR6_Lens",
+    "ACT DR6 Lensing (for combining with primary CMB)": "ACT_DR6_Lens_and_CMB",
+}
+
+
+def print_all_shortcuts():
+    """
+    Prints all available shortcuts to data sets.
+    """
+    for ky in list(shortcuts.keys()):
+        print(
+            f"{ky}: 'candl.data.{shortcuts[ky]}'\n(data files located at: {globals()[shortcuts[ky]]})"
+        )
