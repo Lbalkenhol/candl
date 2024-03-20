c5d8335e234bbdeec4ecd1e7a870b59baf506d1a
Lennart Balkenhol <lennart@MacBook-Pro-90.local>
2024-03-20 14:09:39 +0100

    Added version number to data set folders

--- candl/data/__init__.py
+++ candl/data/__init__.py
@@ -18,12 +18,14 @@ import os
 data_path = os.path.dirname(os.path.realpath(__file__))
 
 # Data Sets
-SPT3G_2018_TTTEEE = f"{data_path}/SPT3G_2018_TTTEEE/SPT3G_2018_TTTEEE.yaml"
-SPT3G_2018_Lens = f"{data_path}/SPT3G_2018_Lens/SPT3G_2018_PP_lens_only.yaml"
-SPT3G_2018_Lens_and_CMB = f"{data_path}/SPT3G_2018_Lens/SPT3G_2018_PP_lens_and_CMB.yaml"
-ACT_DR4_TTTEEE = f"{data_path}/ACT_DR4_CMB_only/ACT_DR4_CMB_only.yaml"
-ACT_DR6_Lens = f"{data_path}/ACT_DR6_Lens/ACT_DR6_KK_lens_only.yaml"
-ACT_DR6_Lens_and_CMB = f"{data_path}/ACT_DR6_Lens/ACT_DR6_KK_lens_and_CMB.yaml"
+SPT3G_2018_TTTEEE = f"{data_path}/SPT3G_2018_TTTEEE_v0/SPT3G_2018_TTTEEE.yaml"
+SPT3G_2018_Lens = f"{data_path}/SPT3G_2018_Lens_v0/SPT3G_2018_PP_lens_only.yaml"
+SPT3G_2018_Lens_and_CMB = (
+    f"{data_path}/SPT3G_2018_Lens_v0/SPT3G_2018_PP_lens_and_CMB.yaml"
+)
+ACT_DR4_TTTEEE = f"{data_path}/ACT_DR4_CMB_only_v0/ACT_DR4_CMB_only.yaml"
+ACT_DR6_Lens = f"{data_path}/ACT_DR6_Lens_v0/ACT_DR6_KK_lens_only.yaml"
+ACT_DR6_Lens_and_CMB = f"{data_path}/ACT_DR6_Lens_v0/ACT_DR6_KK_lens_and_CMB.yaml"
 
 shortcuts = {
     "SPT-3G 2018 TT/TE/EE": "SPT3G_2018_TTTEEE",
