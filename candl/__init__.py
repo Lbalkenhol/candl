440e659e897772c24709021235fa065b2482558a
Lennart Balkenhol <lennart@MacBook-Pro-136.local>
2025-03-24 14:05:34 +0100

    Bump version number

--- candl/__init__.py
+++ candl/__init__.py
@@ -1,7 +1,7 @@
 """Differentiable Likelihood for CMB Analysis"""
 
 __author__ = ["L. Balkenhol", "C. Trendafilova", "K. Benabed", "S. Galli"]
-__version__ = "1.6.0"
+__version__ = "1.7.0"
 __description__ = "Differentiable Likelihood for CMB Analysis."
 
 from .likelihood import *
